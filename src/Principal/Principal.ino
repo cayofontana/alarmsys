d898d06473dd13835d385ec9e0966dc5c2058fb0
cayo <cayofontana>
2018-02-26 04:17:36 -0300

    thread herança

--- src/Principal/Principal.ino
+++ src/Principal/Principal.ino
@@ -1,21 +1,40 @@
+#include <vector>
+
 #include "SensorUltrasonico.h"
 
-SensorUltrasonico sensorUltrasonico(12, 13);
+using namespace std;
+
+vector<SensorUltrasonico> sensoresUltrasonicos;
 const uint8_t pinoSirene = 4;
 
 void setup()
 {
         Serial.begin(115200);
         pinMode(pinoSirene, OUTPUT);
+
+        sensoresUltrasonicos.reserve(2);
+        sensoresUltrasonicos.push_back(SensorUltrasonico(12, 13, 50, 5000));
+        sensoresUltrasonicos.push_back(SensorUltrasonico(5, 2, 50, 5000));
 }
 
 void loop()
 {
-        if (sensorUltrasonico.existeObjeto())
+        bool detectou;
+        
+        for (vector<SensorUltrasonico>::iterator sensorUltrasonico = sensoresUltrasonicos.begin(); sensorUltrasonico != sensoresUltrasonicos.end(); ++sensorUltrasonico)
         {
-                digitalWrite(pinoSirene, HIGH);
-                delay(5000);
+                if (!sensorUltrasonico->detectar())
+                {
+                        detectou = false;
+                        break;
+                }
+                else
+                        detectou = true;
         }
+
+        if (detectou)
+                digitalWrite(pinoSirene, HIGH);
         else
                 digitalWrite(pinoSirene, LOW);
 }
+

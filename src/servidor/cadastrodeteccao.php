e1d60b1929afd1cfc0e2d4435521f934aafda02f
cayofontana <cayofontana@gmail.com>
2018-04-26 12:34:51 -0300

    implementacao mode físico

--- src/servidor/cadastrodeteccao.php
+++ src/servidor/cadastrodeteccao.php
@@ -5,26 +5,25 @@
 	</head>
 	<body>
 		<?php
-		// Obtém o valor enviado pelo leitor via HTTP Request (GET Method).
-		$distancia = $_GET["distancia"];
-		// Cria uma conexão com o SGDB MySQL, utilizando a API MySQLi.
-		$conexao = new mysqli("127.0.0.1", "alarmsys", "abc@123.", "ALARMSYS");
-		// Verifica se ocorreram erros durante a criação da conexão.
-		if (mysqli_connect_errno())
-		{
-			printf("Não foi possível conectar-se ao SGDB MySQL: %s\n", mysqli_connect_error());
-			exit();
-		}
-
-		// Cria uma transação SQL.
-		$transacao = $conexao->prepare("INSERT INTO DETECCAO (DETE_VL_DISTANCIA, DETE_DT_DETECCAO) VALUES (?, NOW());");
-		// Associa, no script de transação SQL, os valores armazenados em variáveis de acordo
-		// com o seu tipo ('i' define que o valor armazenado na variávei é do tipo inteiro).
-		$transacao->bind_param('i', $distancia);
-		// Executa a transação SQL.
-		$transacao->execute();
-		// Fecha a conexão com o SGDB.
-		$conexao->close();
+			// Obtém o valor enviado pelo leitor via HTTP Request (GET Method).
+			$distancia_media = $_GET["distancia_media"];
+			// Cria uma conexão com o SGDB MySQL, utilizando a API MySQLi.
+			$conexao = new mysqli("127.0.0.1", "monitor", "ifes2018", "ALARMSYS");
+			// Verifica se ocorreram erros durante a criação da conexão.
+			if (mysqli_connect_errno())
+			{
+				printf("Não foi possível conectar-se ao SGDB MySQL: %s\n", mysqli_connect_error());
+				exit();
+			}
+			// Cria uma transação SQL.
+			$transacao = $conexao->prepare("INSERT INTO DETECCAO (DETE_VL_DISTANCIA_MEDIA, DETE_DT_DETECCAO) VALUES (?, NOW());");
+			// Associa, no script de transação SQL, os valores armazenados em variáveis de acordo
+			// com o seu tipo ('d' define que o valor armazenado na variável é do tipo double).
+			$transacao->bind_param('d', $distancia_media);
+			// Executa a transação SQL.
+			$transacao->execute();
+			// Fecha a conexão com o SGDB.
+			$conexao->close();
 		?>
 	</body>
 </html>

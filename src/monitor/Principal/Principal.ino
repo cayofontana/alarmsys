b8a464ce2e47f039c0e3879c8b7568223d7e1fcd
cayofontana <cayocruz@gmail.com>
2019-02-14 11:47:32 -0200

    Alteração nos pinos de comunicação entre o ESP8266 e os sensores

--- src/monitor/Principal/Principal.ino
+++ src/monitor/Principal/Principal.ino
@@ -8,9 +8,8 @@ std::vector<std::shared_ptr<Sensor>> sensores;
 Rede rede("CAYO", "abc@123.");
 const uint8_t pinoSirene = 2;
 
-void setup()
-{
-        Serial.begin(9600);
+void setup() {
+        Serial.begin(115200);
         pinMode(pinoSirene, OUTPUT);
         digitalWrite(pinoSirene, LOW);
 
@@ -19,25 +18,21 @@ void setup()
         sensores.push_back(std::make_shared<InfraVermelho>(0, 10, 15000));
 }
 
-void loop()
-{
+void loop() {
         bool objetoDetectado = false;
         
         for (std::vector<std::shared_ptr<Sensor>>::iterator sensor = sensores.begin(); sensor != sensores.end(); ++sensor)
                 (*sensor)->detectar();
 
-        for (std::vector<std::shared_ptr<Sensor>>::iterator sensor = sensores.begin(); sensor != sensores.end(); ++sensor)
-        {
-                if (!(*sensor)->existeObjeto())
-                {
+        for (std::vector<std::shared_ptr<Sensor>>::iterator sensor = sensores.begin(); sensor != sensores.end(); ++sensor) {
+                if (!(*sensor)->existeObjeto()) {
                         objetoDetectado = false;
                         break;
                 }
                 objetoDetectado = true;
         }
 
-        if (objetoDetectado && digitalRead(pinoSirene) == LOW)
-        {
+        if (objetoDetectado && digitalRead(pinoSirene) == LOW) {
                 digitalWrite(pinoSirene, HIGH);
                 if (rede.conectar())
                         rede.enviarDados("192.168.0.102", 8080, "cadastrodeteccao.jsp", 10);

da6257896374ad6b0745b9c2449a2a2d6097524f
cayofontana <cayo@cruzgmail.com>
2019-02-24 01:34:06 +0000

    Construcao da classe Sirene

---
+++ src/servidor/observador/imp/modelo/Sirene.java
@@ -0,0 +1,40 @@
+package imp.modelo;
+
+import com.pi4j.io.gpio.GpioController;
+import com.pi4j.io.gpio.GpioFactory;
+import com.pi4j.io.gpio.GpioPinDigitalOutput;
+import com.pi4j.io.gpio.PinState;
+import com.pi4j.io.gpio.RaspiPin;
+
+
+public class Sirene
+{
+	private boolean status;
+	private GpioPinDigitalOutput pinoSirene;
+
+	public Sirene()
+	{
+		
+		status = false;
+		pinoSirene = GpioFactory.getInstance().provisionDigitalOutputPin(RaspiPin.GPIO_07);
+		pinoSirene.low();
+	}
+
+	public void disparar()
+	{
+		if (!status)
+		{
+			status = !status;
+			pinoSirene.high();
+		}
+	}
+
+	public void silenciar()
+	{
+		if (status)
+		{
+			status = !status;
+			pinoSirene.low();
+		}
+	}
+}

aaf567fe2fbc768730c988fe63d451d96519c893
cayofontana <cayo@cruzgmail.com>
2019-02-24 01:35:25 +0000

    Inclusao do objeto Sirene para seu acionamento quando necessario

--- src/servidor/observador/imp/Principal.java
+++ src/servidor/observador/imp/Principal.java
@@ -3,6 +3,7 @@ package imp;
 import imp.modelo.Observador;
 import imp.modelo.Token;
 import imp.modelo.persistencia.AcessoDados;
+import imp.modelo.Sirene;
 import imp.servico.ServicoMovel;
 
 class Principal
@@ -12,12 +13,16 @@ class Principal
 		AcessoDados acessoDados = new AcessoDados();
 		Observador observador = new Observador(acessoDados);
 		Token token = new Token(acessoDados);
+		Sirene sirene = new Sirene();
 
 		while (true)
 			try
 			{
 				if (observador.estadoMudou(acessoDados))
+				{
+					sirene.disparar();
 					ServicoMovel.enviarMensagem(token.listar(), observador.getDeteccaoReferencia().obterInformacoes(), observador.getDeteccaoReferencia().obterValores());
+				}
 
 				Thread.sleep(5000);
 			}

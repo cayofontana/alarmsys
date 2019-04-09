2620ef872b5798fc417c7233f7384a414932e389
cayofontana <cayocruz@gmail.com>
2019-04-09 12:03:01 -0300

    Reconstrução do método enviarDados()

--- src/monitor/Principal/Rede.cpp
+++ src/monitor/Principal/Rede.cpp
@@ -9,51 +9,29 @@ Rede::Rede(const char *nome, const char *senha)
 bool
 Rede::conectar(void)
 {
-        uint8_t i;
-        
         if (estahConectado())
                 return (true);
-        else
-        {
-                criarConexao();
-                
-                for (i = 0; i < 20; i++)
-                {
-                        if (!estahConectado())
-                        {
-                                delay(500);
-                                continue;
-                        }
 
-                        return (true);
-                }
+        criarConexao();
                 
-                desconectar();
-                
-                return (false);
-        }
+        while (!estahConectado())
+                delay(500);
+        
+        return (true);
 }
 
 bool
 Rede::enviarDados(const char *endereco, uint16_t porta, const char *arquivo, uint16_t valor)
 {
-        WiFiClient cliente;
+        HTTPClient clienteHTTP;
         
-        if (cliente.connect(endereco, porta))
-        {
-                cliente.print(
-                String("GET /") + arquivo + ":" + porta + "?distancia_media=" + valor + " HTTP/1.1\r\n" +
-                "Host: " + endereco + "\r\n" +
-                "Connection: close\r\n" +
-                "\r\n"
-                );
-                
-                return (true);
-        }
+        clienteHTTP.begin((String)"http://" + endereco + (String)":" + porta + (String)"/" + arquivo + (String)"?distancia_media=" + (String)valor);
+        int codigoRetornoHTTP = clienteHTTP.GET();
         
-        cliente.stop();
+        if (codigoRetornoHTTP > 0)
+                Serial.println("Erro:" + (String)clienteHTTP.getString());
         
-        return (false);
+        clienteHTTP.end();
 }
 
 void
